8de5cc59f37791b8a0991420b0132f651224309b
NikkyXO <alexisdame2017@gmail.com>
2023-04-24 18:34:11 +0100

    deployment

--- famapi/settings/database.py
+++ famapi/settings/database.py
@@ -4,11 +4,10 @@ from sqlalchemy.orm import sessionmaker
 import pymysql
 import os
 
-# SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:feFsfZ3bV51O7F9F1ASZ@containers-us-west-89.railway.app:5665/railway"
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:jMAoJVX7Ie6NOYF1GEct@containers-us-west-102.railway.app:6998/railway"
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
+engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True, pool_pre_ping=True, pool_size=10, max_overflow=20,
+                       connect_args={'sslmode': 'require'})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -21,5 +20,6 @@ def get_db():
         yield db
     finally:
         db.close()
+
 
 Base.metadata.create_all(bind=engine)
